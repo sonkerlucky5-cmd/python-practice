335742973b22c1e248358d3cee0639e4e59a2082
sonkerlucky5-cmd <sonkerlucky5@gmail.com>
2026-01-27 21:14:24 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+print("my first program in publish Github")
+print("Hello github")
+#Arithmatic operation
+a = 5
+b = 6
+print("addition", a + b)
+print("subtraction", a - b)
+print("multiplication", a * b)
+print("divide", a / b)
+print("power", a ** b)
