371c23fb5e66b40ba13b2196e96aa9bf10870eea
sonkerlucky5-cmd <224416154+sonkerlucky5-cmd@users.noreply.github.com>
2026-03-27 22:51:59 +0530

    day 3 loops,for loops,while loops

---
+++ day3.py
@@ -0,0 +1,19 @@
+# for loops
+for i in range(100):
+    print(i)    
+# for loops second example
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+# while loop 
+password = "1234"
+
+while True:
+    user = input("Enter password: ")
+    
+    if user == password:
+        print("Access granted")
+        break
+    else:
+        print("Wrong password")
