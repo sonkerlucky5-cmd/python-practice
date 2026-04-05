7cfd713143920d65ca2d97d51f9adfd5606e6f09
sonkerlucky5-cmd <224416154+sonkerlucky5-cmd@users.noreply.github.com>
2026-04-05 23:04:56 +0530

    day-11 Numpy Concept's cleared

--- day11.py
+++ day11.py
@@ -2,5 +2,23 @@
 import numpy as np
 normal_list =[1,2,3,4,5]
 print(normal_list)
+# numpy array creation
 numpy_array = np.array(normal_list)
 print(numpy_array)
+# numpy array operations
+numpy_array = numpy_array + 100
+print(numpy_array)
+# array Creation
+import numpy as np
+a = np.array([1, 2, 3, 4, 5])
+b = np.zeros(5)
+c =np.ones(5)
+d = np.arange(0, 10)
+print(a,b,c,d)
+# shape and reshape
+import numpy as np
+arr = np.array([1,2,3,4,5,6])
+
+print(arr.shape)      # (6,)
+new_arr = arr.reshape(2,3)
+print(new_arr)
